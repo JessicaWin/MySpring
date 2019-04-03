3942b42379325c20a7cf6c1b33a715648d422f70
JessicaWin <baihechenlu@126.com>
2019-04-03 09:11:03 +0800

    xml config spring mvc project

--- spring-bean-name-mapper/src/main/java/com/jessica/controller/BaseController.java
+++ spring-bean-name-mapper/src/main/java/com/jessica/controller/BaseController.java
@@ -3,21 +3,18 @@ package com.jessica.controller;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.AbstractController;
 
-@Controller
-@RequestMapping(value = "/")
 public class BaseController extends AbstractController {
 	private static int counter = 0;
 	private static final String VIEW_INDEX = "home";
 
 	@Override
-	@RequestMapping(method = RequestMethod.GET)
+	@RequestMapping(value = "/base", method = RequestMethod.GET)
 	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 
 		ModelAndView mv = new ModelAndView();
@@ -32,9 +29,8 @@ public class BaseController extends AbstractController {
 	}
 
 	@Override
-	@RequestMapping(value = "/base", method = RequestMethod.GET)
-	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
-			throws Exception {
+	@RequestMapping(value = "/base/1", method = RequestMethod.GET)
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
 		ModelAndView mv = new ModelAndView();
 		ModelMap model = new ModelMap();
 		model.addAttribute("message", "Welcome ");
---
+++ spring-mvc-xml-config/src/main/java/com/jessica/controller/BaseController.java
@@ -0,0 +1,46 @@
+package com.jessica.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+@RequestMapping(value = "/")
+public class BaseController extends AbstractController {
+	private static int counter = 0;
+	private static final String VIEW_INDEX = "home";
+
+	@Override
+	@RequestMapping(method = RequestMethod.GET)
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+		ModelAndView mv = new ModelAndView();
+		ModelMap model = new ModelMap();
+		model.addAttribute("message", "Welcome Base home");
+		model.addAttribute("counter", ++counter);
+		// 模型
+		mv.addObject("message", "test");
+		// 视图
+		mv.setViewName(VIEW_INDEX);
+		return mv;
+	}
+
+	@Override
+	@RequestMapping(value = "/base", method = RequestMethod.GET)
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+		ModelAndView mv = new ModelAndView();
+		ModelMap model = new ModelMap();
+		model.addAttribute("message", "Welcome base base");
+		model.addAttribute("counter", ++counter);
+		// 模型
+		mv.addObject("message", "test");
+		// 视图
+		mv.setViewName(VIEW_INDEX);
+		return mv;
+	}
+}
