d926e8f356b421bb3cca61076522a4eaa62e4e1f
JessicaWin <baihechenlu@126.com>
2019-04-06 22:57:45 +0800

    spring mvc simple url handler

---
+++ spring-simple-url-mapper/src/main/java/com/jessica/controller/SimpleUrlMapperController.java
@@ -0,0 +1,29 @@
+package com.jessica.controller;
+
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.ui.ModelMap;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.AbstractController;
+
+public class SimpleUrlMapperController extends AbstractController {
+	private static int counter = 0;
+	private static final String VIEW_INDEX = "home";
+
+	@Override
+	protected ModelAndView handleRequestInternal(HttpServletRequest request, HttpServletResponse response)
+			throws Exception {
+
+		ModelAndView mv = new ModelAndView();
+		ModelMap model = new ModelMap();
+		model.addAttribute("message", "Welcome ");
+		model.addAttribute("counter", ++counter);
+		// 模型
+		mv.addObject("message", "test");
+		// 视图
+		mv.setViewName(VIEW_INDEX);
+		return mv;
+	}
+}
